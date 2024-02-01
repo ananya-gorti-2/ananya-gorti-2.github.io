01bdd8902f22e9427754f42da66f04a619939db8
vishesh12345678 <vishesh.jain1234567@gmail.com>
2024-02-01 01:05:21 -0500

    finished careers page
    Co-authored-by: jaisharan.a@gmail.com <jaisharan.a@gmail.com>
    Co-authored-by: Yaswanth Kandra <Yaswanth0618@users.noreply.github.com>

--- careers.js
+++ careers.js
@@ -13,7 +13,7 @@ const jobs = [
         image: "Cashier.jpg",
         details:
         "As our Cashier, you play a pivotal role in providing efficient and friendly service, handling transactions with a smile and ensuring a smooth flow at Culinary Harbor.",
-        openPositions: "3",
+        openPositions: "1",
         link: "applypos2.html",
     },
 
@@ -22,7 +22,42 @@ const jobs = [
         image: "waiter.jpg",
         details:
         "Join us as a Waiter/Waitress, where your friendly demeanor and service expertise create memorable dining experiences, making Culinary Harbor a destination for exceptional hospitality.",
+        openPosition: "3",
+        link: "#",
+    },
+
+    {
+        title: "Sous Chef",
+        image: "Sous_Chef.jpg",
+        details: 
+        "As Sous Chef at Culinary Harbor, you'll be the creative force behind our culinary offerings, collaborating with the chef to execute exquisite dishes and maintain kitchen excellence.",
         openPosition: "1",
         link: "#",
+    },
+
+    {
+        title: "Dishwasher",
+        image: "dishwasher.jpg",
+        details:
+        "Join Culinary Harbor as a Dishwasher, playing a crucial role in kitchen operations by ensuring cleanliness, efficiency, and the smooth flow of culinary production.",
+        openPosition: "4",
+        linK: "#",
+    },
+
+    {
+        title: "Prep Cook",
+        image: "Prep_Cook.jpeg",
+        details:
+        "Embark on a culinary journey as a Prep Cook at Culinary Harbor, where your precision and dedication contribute to the creation of flavorful dishes, setting the stage for an exceptional dining experience.",
+        openPosition: "2",
+        link: "#",
     }
-]
+];
+
+const jobsHeading = document.querySelector(".jobs-list-container h2");
+
+if(jobs.length == 1){
+    jobsHeading.innerHTML = `${jobs.length} Job`;
+}else{
+    jobsHeading.innerHTML = `${jobs.length} Jobs`;
+}
