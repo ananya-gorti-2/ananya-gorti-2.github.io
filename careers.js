82e496172441ef314e8366861e098de1afe77b05
Jaisharan <jaisharan3007@gmail.com>
2024-02-01 00:05:00 -0500

    added imgs and txt

---
+++ careers.js
@@ -0,0 +1,28 @@
+const jobs = [
+    {
+        title: "General Manager",
+        image: "General_Manager.jpg",
+        details:
+        "As the General Manager of Culinary Harbor, you will be the key orchestrator of operational excellence overseeing day-to-day restaurant management, ensuring exceptional guest experiences, and leading a dynamic team to culinary success.",
+        openPositions: "2",
+        link: "applypos1.html",
+    },
+
+    {
+        title: "Cashier",
+        image: "Cashier.jpg",
+        details:
+        "As our Cashier, you play a pivotal role in providing efficient and friendly service, handling transactions with a smile and ensuring a smooth flow at Culinary Harbor.",
+        openPositions: "3",
+        link: "applypos2.html",
+    },
+
+    {
+        title: "Waiter/Waitress",
+        image: "waiter.jpg",
+        details:
+        "Join us as a Waiter/Waitress, where your friendly demeanor and service expertise create memorable dining experiences, making Culinary Harbor a destination for exceptional hospitality.",
+        openPosition: "1",
+        link: "#",
+    }
+]
