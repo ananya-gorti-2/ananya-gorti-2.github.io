a7ad87e05f4afa9c6d60b08c58e2903039960149
Jaisharan <jaisharan3007@gmail.com>
2024-02-02 00:03:15 -0500

    added loading screen, and updated icons and text

--- main.js
+++ main.js
@@ -118,3 +118,14 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+function loader(){
+  document.querySelector(".loader-container").classList.add("fade-out");
+}
+
+function fadeOut(){
+  setInterval(loader, 3000);
+
+}
+
+window.onload = fadeOut();
